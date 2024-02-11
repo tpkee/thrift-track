7d2401ac131c24571b9d6bb56dd9a2defc7ef9eb
tpkee <89136677+tpkee@users.noreply.github.com>
2024-02-11 01:17:52 +0100

    feat: add svg loader config and add new spinner.svg icon

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -30,6 +30,28 @@ export default bundleAnalyzer(
         "utf-8-validate": "utf-8-validate",
       });
 
+      const fileLoaderRule = config.module.rules.find((rule) =>
+        rule.test?.test?.(".svg")
+      );
+
+      config.module.rules.push(
+        // Reapply the existing rule, but only for svg imports ending in ?url
+        {
+          ...fileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/, // *.svg?url
+        },
+        // Convert all other *.svg imports to React components
+        {
+          test: /\.svg$/i,
+          issuer: fileLoaderRule.issuer,
+          resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+          use: ["@svgr/webpack"],
+        }
+      );
+
+      fileLoaderRule.exclude = /\.svg$/i;
+
       return config;
     },
   })
